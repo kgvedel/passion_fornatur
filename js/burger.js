69aef3cb9f1d2b5a99e38f8b2f224cb4d39725ee
magn902m <magn902m@stud.kea.dk>
2021-09-10 10:14:16 +0200

    Start på brugermenu

---
+++ js/burger.js
@@ -0,0 +1,47 @@
+"use strict";
+
+// Siden loades
+
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+  document.querySelector("#burger_btn").addEventListener("click", openMenu);
+}
+
+// Open Menu
+
+function openMenu() {
+  const burgerBtn = document.querySelector("#burger_btn");
+  const menu = document.querySelector("#menu");
+  const allStrokes = document.querySelectorAll("#burger_btn .stroke");
+
+  burgerBtn.removeEventListener("click", openMenu);
+
+  burgerBtn.classList = "";
+  allStrokes.forEach(offsetHeigthOnStroke);
+  burgerBtn.classList = "open";
+  menu.classList = "open";
+
+  burgerBtn.addEventListener("click", hideMenu);
+}
+
+// Hide Menu
+function hideMenu() {
+  const burgerBtn = document.querySelector("#burger_btn");
+  const menu = document.querySelector("#menu");
+  const allStrokes = document.querySelectorAll("#burger_btn .stroke");
+
+  burgerBtn.removeEventListener("click", hideMenu);
+
+  burgerBtn.classList = "";
+  allStrokes.forEach(offsetHeigthOnStroke);
+  burgerBtn.classList = "hide";
+  menu.classList = "hide";
+
+  burgerBtn.addEventListener("click", openMenu);
+}
+
+// Kan vi på en eller anden måde optimere hvordan vi genstarter alle animationerne?
+function offsetHeigthOnStroke(elm) {
+  elm.offsetHeight;
+}
