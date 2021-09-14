ec27fd1893208a7881b4acf0d659d1c40803d18e
magn902m <magn902m@stud.kea.dk>
2021-09-14 16:39:41 +0200

    start på popup

--- js/omOs.js
+++ js/omOs.js
@@ -3,21 +3,22 @@
 //Kontroller om DOM´en er loaded
 document.addEventListener("DOMContentLoaded", start);
 
-const knapSumit = document.querySelector(".knap_sumit");
-const popup = document.querySelector("#popup");
-
-function start() {
-  // console.log("start")
-  knapSumit.addEventListener("click", sumitPopup);
-  popup.querySelector(knapSumit).addEventListener("click", sumitPopup);
-}
-
-function sumitPopup() {
-  popup.style.display = "block";
-}
-
 document.querySelector("#popup button").addEventListener("click", lukPopup);
 
 function lukPopup() {
   document.querySelector("#popup").style.display = "none";
+}
+
+function start() {
+  // console.log("start");
+  document.querySelector("#popup button").querySelector("click", klikIndsend);
+  // document
+  //   .querySelector("#popup article")
+  //   .addEventListener("click", () => klikIndsend());
+}
+
+function klikIndsend() {
+  console.log("klikIndsend");
+  const popup = document.querySelector("#popup");
+  popup.style.display = "block";
 }
